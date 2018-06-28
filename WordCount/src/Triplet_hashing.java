b2df9c1c27a542dc3ada22adc47da0eeb0a567e2
khant vyas <khantavyas@gmail.com>
2018-06-28 00:03:10 -0700

    "try hashing"

---
+++ WordCount/src/Triplet_hashing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Triplet_hashing {
+
+	public static void triplate(int[] arry) {
+		boolean var;
+		Map map = new HashMap<Integer,Integer>();
+		
+	
+	
+	}
+	
+	
+	public static void main(String[] args) {
+
+
+		
+		int[] rr = {0,-1,2,-3,1};
+		
+		triplate(rr);
+	}
+
+}
